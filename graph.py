c82e4a95b0479ca1ef78f24b20c8b82536c75652
Daniel Bravo <bravoappdev@gmail.com>
2017-03-07 22:58:14 -0800

    Update add_edge to handle edges that are created explicitly. Minor formatting.

--- graph.py
+++ graph.py
@@ -1,10 +1,13 @@
 class Vertex():
-	current_label = 0 + ord('a') - 1
+	auto_label = 96 # ord('a') - 1
 
 	def __init__(self, graph, x, y, label=None):
-		# super(Vertex, self).__init__()
+		# TODO: Error checking in case the right
+		#		arguement isn't passed in
+
 		# if not isinstance(graph, Graph):
 		# 	return None
+		
 		self.graph = graph
 		self.x = x
 		self.y = y
@@ -22,11 +25,11 @@ class Vertex():
 
 	def auto_gen_label(self, method="vtx"):
 		if method == "abc":
-			Vertex.current_label += 1
-			return chr(Vertex.current_label)
+			Vertex.auto_label += 1
+			return chr(Vertex.auto_label)
 		elif method == "vtx":	
-			Vertex.current_label += 1
-			return "v" + str(Vertex.current_label - 96)
+			Vertex.auto_label += 1
+			return "v" + str(Vertex.auto_label - 96)
 
 	def connect_to_vertex(self, vert, edge):
 		self.adj_vertices[vert.label] = vert
@@ -38,10 +41,9 @@ class Vertex():
 
 
 class Edge():
-	current_label = 0
+	auto_label = 0
 
 	def __init__(self, graph, vert_1, vert_2, label=None, weight=0):
-		# super(Edge, self).__init__()
 		self.graph = graph
 		self.vert_1 = vert_1
 		self.vert_2 = vert_2
@@ -53,19 +55,20 @@ class Edge():
 		self.weight = weight
 		vert_1.connect_to_vertex(vert_2, self)
 		vert_2.connect_to_vertex(vert_1, self)
+		self.graph.add_edge(-1,-1,edge=self)
 
 	def __str__(self):
 		return "Edge  : '{}' with vertices ({}, {}) in Graph '{}'".format(self.label, self.vert_1.label, self.vert_2.label, self.graph.name)
 
 	def auto_gen_label(self, method="edg"):
 		if method == "123":
-			Edge.current_label += 1
-			return str(Edge.current_label)
+			Edge.auto_label += 1
+			return str(Edge.auto_label)
 		if method == "edg":	
-			Edge.current_label += 1
-			return "e" + str(Edge.current_label)
+			Edge.auto_label += 1
+			return "e" + str(Edge.auto_label)
 
-	def are_verts(self, vert_1, vert_2):
+	def has_verts(self, vert_1, vert_2):
 		pass
 
 	def has_loop(self):
@@ -74,8 +77,8 @@ class Edge():
 
 class Graph():
 	graph_count = 0
+	
 	def __init__(self, verts=None, edges=None, name=None):
-		# super(Graph, self).__init__()
 		if verts is None:
 			self.verts = {}
 		else:
@@ -99,21 +102,17 @@ class Graph():
 	def add_vertex(self, x, y, label=None, vert=None):
 		# TODO: Handle adding vert versus creating vert
 		if vert is not None:
-			print "Not none"
-			print vert
-			print vert.label
 			self.verts[vert.label] = vert
+			print vert.label
 			return vert
-		print "me first"
-		v = Vertex(self, x, y, label)
-		print "********* Do I ever get printed????"
-		self.verts[v.label] = v
-		return v
+		return Vertex(self, x, y, label)
 	
-	def add_edge(self, vert_1, vert_2, label=None):
-		e = Edge(self, vert_1, vert_2, label)
-		self.edges[e.label] = e
-		return e
+	def add_edge(self, vert_1, vert_2, label=None, edge=None):
+		if edge is not None:
+			self.edges[edge.label] = edge
+			print edge.label
+			return edge
+		return Edge(self, vert_1, vert_2, label)
 
 	def get_vertex(self, label):
 		return self.verts[label]
