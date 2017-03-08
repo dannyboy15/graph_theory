c48bc9f2e305dafe88ff3d0a3523822f14bce100
Daniel Bravo <bravoappdev@gmail.com>
2017-03-07 18:19:53 -0800

    Added tests to check creation and adding of vertices to a graph.

--- main_graph.py
+++ main_graph.py
@@ -1,15 +1,31 @@
 from graph import *
 
 def main():
-	print "hello world"
 	g = Graph()
-	print g.name
-	v = Vertex(g,1,2)
-	v1 = g.add_vertex(1,1)
-	v2 = g.add_vertex(2,2)
-	print v
-	print v1
-	print v2
+	# print g.name
+	v1 = Vertex(g,1,2)
+	v2 = Vertex(g,1,2)
+	# v3 = Vertex(g,1,2)
+	v3 = g.add_vertex(1,1)
+	# v2 = g.add_vertex(2,2)
+	e1 = g.add_edge(v1, v2)
+	v_1 = g.get_vertex("v1")
+	e_1 = g.get_edge("e1")
+	# print g.verts
+	# print v1
+	# print v2
+	# print e1
+
+	# print v_1
+	# print e_1
+
+	# print "\nPrinting Graph:"
+	# print g
+	# print g.verts
+	for v in g.verts:
+		print g.verts[v].graph
+	for v, v1 in g.verts.items():
+		print v1.graph
 
 if __name__ == '__main__':
 	main()
